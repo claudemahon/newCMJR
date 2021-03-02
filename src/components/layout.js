98c2b595d909f72ed7d859c0782cab130b6dd8ef
claudemahon <claudemahonjr@gmail.com>
2021-03-02 18:58:18 -0500

    made small changes to the site

--- src/components/layout.js
+++ src/components/layout.js
@@ -2,6 +2,7 @@ import React from 'react'
 import 'bulma/css/bulma.css'
 import Navbar from './Navbar.js'
 import Footer from './Footer'
+import '../pages/style.css'
 
 const Layout = ({children}) => {
   return(
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,6 @@
 import React from "react"
 // import { Link } from "gatsby"
 import 'bulma/css/bulma.css'
-import './style.css'
 // Page Components
 import Layout from '../components/Layout'
 import SEO from "../components/seo"
@@ -13,17 +12,17 @@ const IndexPage = () => (
    <SEO title="CMJR Development Home"/>
     <section className="hero is-fullheight bg-img">
      <div className="hero-body">
-       <div className="container">
-         {/* <h1 className="title has-text-light">Agramith</h1> */}
-         <h2 className="subtitle has-text-light ">Making Contracts Great Again</h2>
+       <div className="container has-text-centered">
+         <h2 className="subtitle has-text-light">Better Landlords, Better Tenants, Better Communities</h2>
+         <h1 className="title has-text-light">Agramith</h1>
        </div>
      </div>
    </section>
 
    <section className="section is-large">
      <div className="body">
-       <h1 className="title">What is Agramith?</h1>
-       <h2 className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur reprehenderit aliquam, rem corporis aliquid, quis cumque quam nisi praesentium animi a molestias est totam quod, molestiae repellendus odio harum repellat eligendi asperiores. Cum dolorum sint, architecto ipsum in perferendis accusantium magnam eaque vero aut. Est eligendi harum a, unde, expedita numquam similique voluptatum praesentium, dignissimos illum recusandae reprehenderit! Accusamus consectetur eos pariatur nihil reiciendis tenetur veniam repudiandae eligendi ea, perferendis qui quaerat in quisquam. Vero dolorem minus impedit delectus cumque necessitatibus perspiciatis in quibusdam voluptates et dolorum suscipit id odio unde tenetur, aspernatur eaque dignissimos quis accusantium! Quam, cum labore. </h2>
+       <h1 className="title">Ag - ra - mi - th</h1>
+       <h2 className="subtitle">Agramith is currently in the works. Agramith is being developed to harness the power of the blockchain to help landlords better manage their properties. Allow Landlords to select from a pool of vetted and interested tenants. Allow tenants to view the ratings of the landlord, all while harnessing the power of Smart Contracts. Procced to the About us for a more indepht look at Agramith</h2>
      </div>
    </section>
    </Layout>
