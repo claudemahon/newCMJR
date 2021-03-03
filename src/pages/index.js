e15dca1a51c2ba94c1f3d3cf7308d3ea8661ec32
claudemahon <claudemahonjr@gmail.com>
2021-03-02 20:37:25 -0500

    changed the rules of the platform

--- src/pages/index.js
+++ src/pages/index.js
@@ -13,8 +13,8 @@ const IndexPage = () => (
     <section className="hero is-fullheight bg-img">
      <div className="hero-body">
        <div className="container has-text-centered">
-         <h2 className="subtitle has-text-light">Better Landlords, Better Tenants, Better Communities</h2>
-         <h1 className="title has-text-light">Agramith</h1>
+         <h2 className="subtitle has-text-light">Making everyone a landlord</h2>
+         {/* <h1 className="title has-text-light">Agramith</h1> */}
        </div>
      </div>
    </section>
@@ -22,7 +22,7 @@ const IndexPage = () => (
    <section className="section is-large">
      <div className="body">
        <h1 className="title">Ag - ra - mi - th</h1>
-       <h2 className="subtitle">Agramith is currently in the works. Agramith is being developed to harness the power of the blockchain to help landlords better manage their properties. Allow Landlords to select from a pool of vetted and interested tenants. Allow tenants to view the ratings of the landlord, all while harnessing the power of Smart Contracts. Procced to the About us for a more indepht look at Agramith</h2>
+       <h2 className="subtitle">Keeping both landlord and tenants in mind, Agramith is being developed to meet the very demanding needs of both landlords and tenants. The Agramith platform is being developed to initally deal with, payment of rent, provide a recorded environment were all communiscations are monitored. Porvide a large service network for landlords and provide both landlords and tenats a way of verifying each other. This platform is being developed to help improve the experience of private and commercial real estate.</h2>
      </div>
    </section>
    </Layout>
