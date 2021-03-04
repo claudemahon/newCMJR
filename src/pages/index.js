37307ed6334fa021ad476dfb220cb4cf1af39dde
claudemahon <claudemahonjr@gmail.com>
2021-03-04 18:18:20 -0500

    upgraded gatsby to v3

--- src/pages/index.js
+++ src/pages/index.js
@@ -22,7 +22,7 @@ const IndexPage = () => (
    <section className="section is-large">
      <div className="body">
        <h1 className="title">Ag - ra - mi - th</h1>
-       <h2 className="subtitle">Keeping both landlord and tenants in mind, Agramith is being developed to meet the very demanding needs of both landlords and tenants. The Agramith platform is being developed to initally deal with, payment of rent, provide a recorded environment were all communiscations are monitored. Porvide a large service network for landlords and provide both landlords and tenats a way of verifying each other. This platform is being developed to help improve the experience of private and commercial real estate.</h2>
+       <h2 className="subtitle">Agramith is being developed to help both landlords and tenants reach common ground. Agramith is currently being developed to utilize smart contracts to better automate commerical real estate. </h2>
      </div>
    </section>
    </Layout>
