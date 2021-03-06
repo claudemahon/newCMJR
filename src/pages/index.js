a5539b0fb900b227b222c9710fbb6e9b5055794b
claudemahon <claudemahonjr@gmail.com>
2021-03-06 11:51:40 -0500

    upgraded all packeages to v3

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,4 @@
 import React from "react"
-// import { Link } from "gatsby"
 import 'bulma/css/bulma.css'
 // Page Components
 import Layout from '../components/Layout'
@@ -21,7 +20,7 @@ const IndexPage = () => (
 
    <section className="section is-large">
      <div className="body">
-       <h1 className="title">Ag - ra - mi - th</h1>
+       <h1 className="title">Agramith</h1>
        <h2 className="subtitle">Agramith is being developed to help both landlords and tenants reach common ground. Agramith is currently being developed to utilize smart contracts to better automate commerical real estate. </h2>
      </div>
    </section>
