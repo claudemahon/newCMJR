1a4554aeb0e2b77bc3d8426dd1f4f372fa73ac4a
claudemahon <claudemahonjr@gmail.com>
2021-03-07 10:01:42 -0500

    There was an error in the file name Layout.js

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import 'bulma/css/bulma.css'
 import Navbar from './Navbar.js'
-import Footer from './Footer'
+import Footer from './Footer.js'
 import '../pages/style.css'
 
 const Layout = ({children}) => {
