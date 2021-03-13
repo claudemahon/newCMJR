0d9942bba57d317462940a6c0bcb065de72e0704
claudemahon <claudemahonjr@gmail.com>
2021-03-13 16:25:45 -0500

    fixed navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,14 +1,37 @@
-import React from 'react';
+import React, {useEffect} from 'react';
 import 'bulma/css/bulma.css';
 import {Link} from 'gatsby';
 
 const Navbar = () => {
+    useEffect(() => {
+        let burger = document.getElementById(`burger`)
+        let menu = document.getElementById(`menu`)
+        burger.addEventListener(`click`, function () {
+          burger.classList.toggle(`is-active`)
+          menu.classList.toggle(`is-active`)
+        })
+      })
     return(
         <>
-        <nav className="navbar transparent is-fixed-top">
+        <nav className="navbar is-fixed-top">
             <div className="navbar-brand">
                 <Link className="navbar-item nav-title"to="#">Claude Mahon</Link>
-            </div>
+                <div role="button"
+                    id="burger"
+                    className="navbar-burger burger"
+                    aria-label="menu"
+                    aria-expanded="false"
+                    data-target="navbarBasicExample">
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                    <span aria-hidden="true"></span>
+                </div>
+                </div>
+                <div className="navbar-menu" id="menu">
+                    <div className="navbar-end">
+                        <Link className="navbar-item" to="#">Projects</Link>
+                    </div>
+                </div>
         </nav>
         </>
     )
