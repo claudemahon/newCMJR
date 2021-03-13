0eecf382aa3625e8b3c163588022abc298399d40
claudemahon <claudemahonjr@gmail.com>
2021-03-13 17:29:36 -0500

    finished the home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -20,8 +20,8 @@ const IndexPage = () => (
 
    <section className="section is-large">
      <div className="body has-text-centered">
-       <h1 className="main-title">About Me</h1>
-       <h2 className="subtitle">I design and code beautifully simple things, and I love what I do.</h2>
+       <h1 className="main-title">I design and code beautifully simple things, and I love what I do. </h1>
+       <h2 className="subtitle">Hi there, I'm Claude from Toronto. I have been developing website's for around 6 years as a hobby, with 1 year of professional web development. After graduating from Highschool, I attended and graduated from Humber College's Web Design and Development program. Please, feel free to contact me if you see anything you like. Please navigate to my project page to view some of my creations.</h2>
      </div>
    </section>
    </Layout>
