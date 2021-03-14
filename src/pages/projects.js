f7a101d5e715c971146d615f6e278e3c92a35b45
claudemahon <claudemahonjr@gmail.com>
2021-03-13 20:38:45 -0500

    added project page

---
+++ src/pages/projects.js
@@ -0,0 +1,24 @@
+import react from 'react'
+import 'bulma/css/bulma.css'
+import SEO from '../components/seo'
+import Layout from '../components/Layout'
+
+
+const Projects = () => {
+    return(
+        <>
+        <SEO title='Products'/>
+        <Layout>
+            <section className="hero is-fullheight">
+                <div className="hero-body">
+                    <div className="container">
+                        <h1 className="main-title">Page in Development</h1>
+                    </div>
+                </div>
+            </section>
+        </Layout>
+        </>
+    )
+}
+
+export default Projects
